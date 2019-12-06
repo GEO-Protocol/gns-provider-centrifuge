4fdc58e7e3a46e7946494d41e085cb0a00b283f4
Eugene Minov <minov.eug@gmail.com>
2019-12-06 22:50:08 +0200

    Implementing stress test with 3000 nodes pinging and lookup-ing continuously with interval of 10 seconds

--- tests/tests_stress.py
+++ tests/tests_stress.py
@@ -17,7 +17,7 @@ class TestsStress(unittest.TestCase):
         self.core.run_threads()
         time.sleep(0.5)
 
-        self.test_clients_count = 100
+        self.test_clients_count = 3000
         self.test_clients_start_id = 1234567000
         self.test_clients = []
         print("Creating "+str(self.test_clients_count)+" test clients...")
@@ -59,6 +59,20 @@ class TestsStress(unittest.TestCase):
         hours, rem = divmod(time.time() - start_time, 3600)
         minutes, seconds = divmod(rem, 60)
         print("Finished in {:0>2}:{:0>2}:{:05.2f}".format(int(hours), int(minutes), seconds))
+
+    def test_stress(self):
+        while True:
+            for test_client in self.test_clients:
+                client_pkg.send_ping(self.settings.ping_host, self.settings.ping_port, test_client.test_client_id, False)
+                time.sleep(0.001)
+
+            for test_client in self.test_clients:
+                address = client_pkg.send_lookup(
+                    self.settings.provider_name, self.settings.host, self.settings.port,test_client.test_client_username,
+                        self.settings.gns_address_separator, True, 5)
+                #if address == None:
+                #    assert False, "Lookup failed"
+                time.sleep(0.001)
 
     class __TestClient:
         def __init__(self, client_manager, id):
