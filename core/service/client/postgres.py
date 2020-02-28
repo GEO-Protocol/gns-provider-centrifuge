869856f9fa5c9fe60632dc5ea8cc159312c38607
Eugene Minov <minov.eug@gmail.com>
2020-02-28 15:53:00 +0200

    Fixing posgtres connection loss over time

--- core/service/client/postgres.py
+++ core/service/client/postgres.py
@@ -10,18 +10,13 @@ from core.settings import Settings
 
 class Manager(interface.Manager):
     def __init__(self, settings: Settings):
+        self.settings = settings
         self.redis = redis.Manager(settings)
 
         self._conn = None
+        self._cur = None
+        self.connect()
         try:
-            self._conn = psycopg2.connect(
-                host=settings.postgres.host,
-                port=settings.postgres.port,
-                database=settings.postgres.database,
-                user=settings.postgres.user,
-                password=settings.postgres.password
-            )
-            self._cur = self._conn.cursor()
             self._cur.execute(
                 """
                 CREATE TABLE IF NOT EXISTS client (
@@ -39,7 +34,28 @@ class Manager(interface.Manager):
         # client = self.create(1, "minyor")
         # self.save(client)
 
+    def connect(self):
+        try:
+            self._conn = psycopg2.connect(
+                host=self.settings.postgres.host,
+                port=self.settings.postgres.port,
+                database=self.settings.postgres.database,
+                user=self.settings.postgres.user,
+                password=self.settings.postgres.password
+            )
+            self._cur = self._conn.cursor()
+        except (Exception, psycopg2.DatabaseError) as error:
+            self._conn = None
+            print(error)
+
+    def check_for_connection(self):
+        try:
+            self._conn.execute('SELECT 1')
+        except psycopg2.OperationalError:
+            self.connect()
+
     def find_by_id(self, id):
+        self.check_for_connection();
         self._cur.execute(
             'SELECT * from client where id = %s', (str(id),)
         )
@@ -50,6 +66,7 @@ class Manager(interface.Manager):
         return self.redis.load(client)
 
     def find_by_username(self, username):
+        self.check_for_connection();
         self._cur.execute(
             'SELECT * from client where username = %s', (username,)
         )
@@ -68,6 +85,7 @@ class Manager(interface.Manager):
         self.redis.save(client)
         if redis_only:
             return
+        self.check_for_connection();
         existing_client = self.find_by_id(client.id)
         if existing_client:
             self._cur.execute(
@@ -85,6 +103,7 @@ class Manager(interface.Manager):
         self.redis.delete(client)
         if redis_only:
             return
+        self.check_for_connection();
         self._cur.execute(
             "delete from client where id=%s;",
             (client.id,)
